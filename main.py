53acc8064cc270dbd2a38c990f5bb9ebf772eed7
Ippei Kanetsuki <one.flat0402@gmail.com>
2024-10-24 00:23:59 +0900

    no message

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
-from typing import Optional
-import os
+# from typing import List
+# import os
+# import datetime
 
 # Azure database for MySQLへの接続用のモジュールをインポート
-import mysql.connector
-from mysql.connector import errorcode
+# import mysql.connector
+# from mysql.connector import errorcode
 
-# .envファイルから環境変数を読み込む
+# # .envファイルから環境変数を読み込む
 # dotenv_path = join(dirname(__file__), ".env")
 # load_dotenv(dotenv_path)
 
@@ -46,14 +47,37 @@ app = FastAPI(
 # テスト用の変数
 # code = "363349884013"
 
+# リクエストボディの定義　パターン１
+# class PurchaseHistory(BaseModel):
+#     emp_cd: str
+#     store_cd: str
+#     pos_no: str
+#     prd_id: str
+#     prd_code: str
+#     prd_name: str
+#     prd_price: int
+
+# リクエストボディの定義　パターン2
+# class Product(BaseModel):
+#     prd_id: str
+#     prd_code: str
+#     prd_name: str
+#     prd_price: int
+# class PurchaseHistory(BaseModel):
+#     emp_cd: str
+#     store_cd: str
+#     pos_no: str
+#     products: List[Product] # 複数の商品をリストで受け取る
+
 # テスト用のAPI
 @app.get("/")
 def test():
     return {"Hello": "World"}
 
-# 商品情報取得用のAPI
+# 非同期処理にした方がよさそう。
+# # 商品情報取得用のAPI
 # @app.get("/get-products/{code}", summary="商品情報取得API")
-# def read_items(code):
+# def read_items(code: str):
 #     try:
 #         conn = mysql.connector.connect(**config)
 #         print("Connection established")
@@ -86,3 +110,50 @@ def test():
 #         print("Done.")
         
 #         return {"prd_id": prd_id, "code":code, "name": name, "price": price}
+
+# # 購入履歴登録用のAPI
+# @app.post("/post-histories", summary="購入履歴登録API")
+# def add_histories(histories: List[PurchaseHistory]): # パターン１のリクエストボディにした場合
+#     try:
+#         conn = mysql.connector.connect(**config)
+#         print("Connection established")
+#     except mysql.connector.Error as err:
+#         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#             print("Something is wrong with the user name or password")
+#         elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#             print("Database does not exist")
+#         else:
+#             print(err)
+#     # DBへ正常に接続できたときの処理
+#     else:
+#         dt_now = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+#         cursor = conn.cursor()
+#         # Insert some data into table.取引テーブルの行追加
+#         cursor.execute(f"""INSERT INTO t_trd (DATETIME, TOTAL_AMT, TTL_AMT_EX_TAX) VALUES ("{dt_now}", 0, 0);""")
+#         cursor.execute("SELECT LAST_INSERT_ID();")
+#         trd_id = cursor.fetchone()[0]
+
+#     #     # 取引明細テーブルへの登録 および 合計と税金額の計算
+#         i = 1
+#         v_sum_notax = 0
+#         tax_rate = 0.1 # 消費税率 *本当は税マスタから取得する
+#         for history in histories:
+#             cursor.execute("""
+#                            INSERT INTO t_trd_dtl (TRD_ID, DTL_ID, PRD_ID, PRD_CODE, PRD_NAME, PRD_PRICE, TAX_CD) VALUES (%s, %s, %s, %s, %s, %s, %s);
+#                            """,
+#                            (trd_id, i, history.prd_id, history.prd_code, history.prd_name, history.prd_price, "10"))
+#             v_sum_notax += history.prd_price
+#             i += 1
+#         v_sum = int(v_sum_notax * (1+tax_rate))
+
+#     #     # Update a data row in the table.取引テーブルの更新
+#         cursor.execute("UPDATE t_trd SET TOTAL_AMT = %s, TTL_AMT_EX_TAX = %s WHERE TRD_ID = %s;", (v_sum,v_sum_notax, trd_id))
+#         print("Updated",cursor.rowcount,"row(s) of data.")
+
+#         # Cleanup
+#         conn.commit()
+#         cursor.close()
+#         conn.close()
+#         print("Done.")
+
+#         return {"total_amt": v_sum, "total_amt_notax":v_sum_notax}
